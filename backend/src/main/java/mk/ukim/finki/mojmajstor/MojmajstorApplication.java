06a1f68843436da308fffe8db822ef77ea55c57e
=Stefan Stoichkov <=>
2024-08-30 14:47:42 +0200

    fix

---
+++ backend/src/main/java/mk/ukim/finki/mojmajstor/MojmajstorApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.mojmajstor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojmajstorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojmajstorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/mk/ukim/finki/mojmajstor/MojmajstorApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.mojmajstor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojmajstorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
